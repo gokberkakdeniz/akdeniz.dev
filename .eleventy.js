29a62c6d1237fa0ccf482297c33e62eedc3c369b
Gökberk AKDENİZ <gokberkakdeniz@hotmail.com>
2021-05-12 00:48:12 +0300

    feat: meta tags added, output prettified

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const prettier = require("prettier");
+const path = require("path");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
-  
+
   eleventyConfig.addPassthroughCopy({ "public": "/" });
 
   eleventyConfig.addLayoutAlias("default", "layouts/base.njk");
@@ -13,6 +15,14 @@ module.exports = function (eleventyConfig) {
     'dateFormat',
     require('./plugins/dateFormat.js')
   );
+
+  eleventyConfig.addTransform("prettier", function (content, outputPath) {
+    if (outputPath && outputPath.endsWith(".html")) {
+      return prettier.format(content, { parser: "html", printWidth: Infinity });
+    } else {
+      return content;
+    }
+  });
 
   return {
     dir: {
